a1a6ecc8edc36d34332275ec2283594ac29e96bb
Junkes887 <henriquejunkes887@gmail.com>
2022-06-07 18:22:40 -0300

    Regras de negocio

--- src/main/java/br/com/esucri/vendacarros/services/CarroService.java
+++ src/main/java/br/com/esucri/vendacarros/services/CarroService.java
@@ -1,10 +1,15 @@
 package br.com.esucri.vendacarros.services;
 
 import br.com.esucri.vendacarros.entities.Carro;
+import br.com.esucri.vendacarros.exceptions.ErroMessage;
+import br.com.esucri.vendacarros.exceptions.RestException;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.core.Response;
 
 @Stateless
 public class CarroService {
@@ -17,15 +22,33 @@ public class CarroService {
     }
 
     public Carro add(Carro carro) {
+        validaPreco(carro);
         entityManager.persist(carro);
         return carro;
     }
+    
+    private String validaPreco(Carro carro) {
+        if (carro.getPreco().compareTo(BigDecimal.ZERO) != 1) {
+            throw new RestException(new ErroMessage("O preço do carro não pode ser menor ou igual a 0!"), Response.Status.BAD_REQUEST);
+        }
+        return "";
+    }
+    
+    private String validaVendido(Carro carro) {
+        if (carro.getVendido()) {
+            throw new RestException(new ErroMessage("O carro já foi vendido não podendo ser excluido!"), Response.Status.BAD_REQUEST);
+        }
+        return "";
+    }
+    
 
     public void remove(Long id, Carro carro) {
+        validaVendido(carro);
         entityManager.remove(findById(id));
     }
 
     public Carro update(Carro carroAtualizado) {
+        validaPreco(carroAtualizado);
         entityManager.merge(carroAtualizado);
         return carroAtualizado;
     }
